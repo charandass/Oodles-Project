edbe0b4ef60d742082014de5da1bfd239e974df7
charandass <charan1631@gmail.com>
2020-03-15 10:46:27 +0530

    Oodles Machine test

---
+++ javascript/index.js
@@ -0,0 +1,2 @@
+$("#view-all-services").click(function(){$(".popup-service-container").css("display","block")}),
+$(".remove-popup").click(function(){$(".popup-service-container").css("display","none")});
